c2cdc451305c6b50443ea35b6f4517214c9d34d3
Artur Santos Nascimento <arturxz@outlook.com.br>
2018-07-12 10:03:09 -0300

    teste de tamanhos das estruturas adicionado

---
+++ testCode/get_struct_sizes.cl
@@ -0,0 +1,39 @@
+typedef struct VglClStrEl{ 
+    float data[256];
+    int ndim;
+    int shape[20];
+    int offset[20];
+    int size;
+} VglClStrEl;
+
+typedef struct VglClShape{ 
+    int ndim;
+    int shape[20];
+    int offset[20];
+    int size;
+} VglClShape;
+
+__kernel void get_struct_sizes( __global uint *struct_sizes )
+{
+    const uint global_id = get_global_id(0u)+get_global_id(1u)*get_global_size(0u);
+    VglClStrEl vgclstrel;
+    VglClShape vgclshape;
+    uint offset;
+
+    printf("In GPU (probing):\n Kernel instance = %d\n", global_id);
+
+    if (global_id==0) {
+        offset = (uint)&(vgclstrel.data);
+        struct_sizes[0] = (uint)sizeof(vgclstrel);
+        struct_sizes[1] = (uint)&(vgclstrel.ndim)-offset;
+        struct_sizes[2] = (uint)&(vgclstrel.shape)-offset;
+        struct_sizes[3] = (uint)&(vgclstrel.offset)-offset;
+        struct_sizes[4] = (uint)&(vgclstrel.size)-offset;
+        offset = (uint)&(vgclshape.ndim);
+        struct_sizes[5] = (uint)sizeof(vgclshape);
+        struct_sizes[6] = (uint)&(vgclshape.shape)-offset;
+        struct_sizes[7] = (uint)&(vgclshape.offset)-offset;
+        struct_sizes[8] = (uint)&(vgclshape.size)-offset;
+    }
+    return;
+}
---
+++ testCode/get_struct_sizes.py
@@ -0,0 +1,79 @@
+from skimage import io
+import matplotlib.pyplot as mp
+import pyopencl as cl
+import numpy as np
+import sys
+
+"""
+	img:
+		is the input image
+	img_shape:
+		3D Images:
+			The OpenCL's default is to be (img_width, img_height, img_depht)
+		2D Images:
+			The The OpenCL's default is to be (img_width, img_height)
+	img_pitch:
+		3D Images (needed):
+			The OpenCL's default is to be (img_width*bytes_per_pixel, img_height*img_width*bytes_per_pixel)
+			and it is assumed if pitch=(0, 0) is given
+		2D Images (optional):
+			The OpenCL's default is to be (img_width*bytes_per_pixel)
+			and it is assumed if pitch=(0) is given
+	img_origin
+		Is the origin of the image, where to start copying the image.
+		2D images must have 0 in the z-axis
+	img_region
+		Is where to end the copying of the image.
+		2D images must have 1 in the z-axis 
+"""
+
+class vgl:
+	# THE vgl CONSTRUCTOR CREATES A NEW CONTEXT
+	# AND INITIATES THE QUEUE, ADDING QUE CONTEXT TO IT.
+	def __init__(self):
+		print("Starting OpenCL")
+		self.platform = cl.get_platforms()[0]
+		self.devs = self.platform.get_devices()
+		self.device = self.devs[0]
+		self.ctx = cl.Context([self.device])
+		#self.ctx = cl.create_some_context()
+		self.queue = cl.CommandQueue(self.ctx)
+		self.builded = False
+
+	# THIS FUNCTION WILL LOAD THE KERNEL FILE
+	# AND BUILD IT IF NECESSARY.
+	def loadCL(self, filepath):
+		print("Loading OpenCL Kernel")
+		self.kernel_file = open(filepath, "r")
+
+		if ((self.builded == False)):
+			print("Building OpenCL kernel")
+			self.pgr = cl.Program(self.ctx, self.kernel_file.read())
+			self.pgr.build()
+			self.kernel_file.close()
+			self.builded = True
+		else:
+			print("Kernel already builded. Going to next step...")
+
+	def execute(self):
+		# CREATING NUMPY ARRAY
+		self.struct_sizes_host = np.zeros(9, np.uint8)
+
+		self.mf = cl.mem_flags
+		self.struct_sizes_device = cl.Buffer( self.ctx, self.mf.READ_ONLY | self.mf.COPY_HOST_PTR, hostbuf=self.struct_sizes_host )
+
+		# EXECUTING KERNEL WITH THE IMAGES
+		print("Executing kernel")
+		self.pgr.get_struct_sizes(self.queue, self.struct_sizes_host.shape, None, self.struct_sizes_device)
+
+		cl.enqueue_copy(self.queue, self.struct_sizes_host, self.struct_sizes_device)
+
+		print("########## GPU RESPONSE ##########")
+		print(self.struct_sizes_host)
+
+		
+
+CLPath = "get_struct_sizes.cl"
+process = vgl()
+process.loadCL(CLPath)
+process.execute()
