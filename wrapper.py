82409c3cfad5436263e31c1e002b54b885d274c4
Artur Santos Nascimento <arturxz@outlook.com.br>
2019-01-19 16:12:22 -0300

    added vglClCopy, vglClDilate and vglClErode to cl2py_CL class

--- wrapper.py
+++ wrapper.py
@@ -103,6 +103,103 @@ class cl2py_CL:
 
 			vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
 
+	def vglClCopy(self, img_input, img_output):
+
+		if( vl.vglCheckContext(img_input, vl.VGL_CL_CONTEXT()) == vl.VGL_ERROR() ):
+			exit()
+		elif( vl.vglCheckContext(img_output, vl.VGL_CL_CONTEXT()) == vl.VGL_ERROR() ):
+			exit()
+		else:
+			self.load_kernel("../CL/vglClCopy.cl", "vglClCopy")
+			kernel_run = self._program.vglClCopy
+
+			kernel_run.set_arg(0, img_input.get_oclPtr())
+			kernel_run.set_arg(1, img_output.get_oclPtr())
+			
+			ev = cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_oclPtr().shape, None)
+			print(ev)
+
+			vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
+
+	def vglClDilate(self, img_input, img_output, convolution_window, window_size_x, window_size_y):
+		
+		if( vl.vglCheckContext(img_input, vl.VGL_CL_CONTEXT()) == vl.VGL_ERROR() ):
+			exit()
+		elif( vl.vglCheckContext(img_output, vl.VGL_CL_CONTEXT()) == vl.VGL_ERROR() ):
+			exit()
+		elif( not isinstance(convolution_window, cl.Buffer) ):
+			print("vglClDilate: Error: convolution_window is not cl.Buffer object.")
+			exit()
+		elif( not isinstance(window_size_x, np.uint32) ):
+			print("vglClDilate: Warning: window_size_x not np.uint32! Trying to convert...")
+			try:
+				window_size_x = np.uint32(window_size_x)
+			except Exception as e:
+				print("vglClDilate: Error!! Impossible to convert window_size_x as a np.uint32 object.")
+				print(str(e))
+				exit()
+		elif( not isinstance(window_size_y, np.uint32) ):
+			print("vglClDilate: Warning: window_size_y not np.uint32! Trying to convert...")
+			try:
+				window_size_y = np.uint32(window_size_y)
+			except Exception as e:
+				print("vglClDilate: Error!! Impossible to convert window_size_y as a np.uint32 object.")
+				print(str(e))
+				exit()
+		else:
+			self.load_kernel("../CL/vglClDilate.cl", "vglClDilate")
+			kernel_run = self._program.vglClDilate
+
+			kernel_run.set_arg(0, img_input.get_oclPtr())
+			kernel_run.set_arg(1, img_output.get_oclPtr())
+			kernel_run.set_arg(2, convolution_window)
+			kernel_run.set_arg(3, window_size_x)
+			kernel_run.set_arg(4, window_size_y)
+
+			ev = cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_oclPtr().shape, None)
+			print(ev)
+
+			vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
+
+	def vglClErode(self, img_input, img_output, convolution_window, window_size_x, window_size_y):
+		
+		if( vl.vglCheckContext(img_input, vl.VGL_CL_CONTEXT()) == vl.VGL_ERROR() ):
+			exit()
+		elif( vl.vglCheckContext(img_output, vl.VGL_CL_CONTEXT()) == vl.VGL_ERROR() ):
+			exit()
+		elif( not isinstance(convolution_window, cl.Buffer) ):
+			print("vglClErode: Error: convolution_window is not cl.Buffer object.")
+			exit()
+		elif( not isinstance(window_size_x, np.uint32) ):
+			print("vglClErode: Warning: window_size_x not np.uint32! Trying to convert...")
+			try:
+				window_size_x = np.uint32(window_size_x)
+			except Exception as e:
+				print("vglClErode: Error!! Impossible to convert window_size_x as a np.uint32 object.")
+				print(str(e))
+				exit()
+		elif( not isinstance(window_size_y, np.uint32) ):
+			print("vglClErode: Warning: window_size_y not np.uint32! Trying to convert...")
+			try:
+				window_size_y = np.uint32(window_size_y)
+			except Exception as e:
+				print("vglClErode: Error!! Impossible to convert window_size_y as a np.uint32 object.")
+				print(str(e))
+				exit()
+		else:
+			self.load_kernel("../CL/vglClErode.cl", "vglClErode")
+			kernel_run = self._program.vglClErode
+
+			kernel_run.set_arg(0, img_input.get_oclPtr())
+			kernel_run.set_arg(1, img_output.get_oclPtr())
+			kernel_run.set_arg(2, convolution_window)
+			kernel_run.set_arg(3, window_size_x)
+			kernel_run.set_arg(4, window_size_y)
+
+			ev = cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_oclPtr().shape, None)
+			print(ev)
+
+			vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
 
 """
 	HERE FOLLOWS THE KERNEL CALLS
@@ -111,6 +208,17 @@ if __name__ == "__main__":
 	
 	# vglClBlurSq3
 	wrp = cl2py_CL()
+
+	img_input_morph = vl.VglImage(sys.argv[1], vl.VGL_IMAGE_2D_IMAGE())
+	vl.vglLoadImage(img_input_morph)
+	if( img_input_morph.getVglShape().getNChannels() == 3 ):
+		vl.rgb_to_rgba(img_input_morph)
+	
+	vl.vglClUpload(img_input_morph)
+
+	img_output_morph = vl.create_blank_image_as(img_input_morph)
+	img_output_morph.set_oclPtr( vl.get_similar_oclPtr_object(img_input_morph) )
+	vl.vglAddContext(img_output_morph, vl.VGL_CL_CONTEXT())
 
 	img_input = vl.VglImage("", vl.VGL_IMAGE_2D_IMAGE())
 	vl.vglLoadImage(img_input, sys.argv[1])
@@ -123,13 +231,26 @@ if __name__ == "__main__":
 	img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
 	vl.vglAddContext(img_output, vl.VGL_CL_CONTEXT())
 
+	convolution_window_morph = np.ones((3, 3), np.float32)
+	convolution_window_morph[0,1] = np.float32(0)
+	convolution_window_morph[1,0] = np.float32(0)
+	convolution_window_morph[1,1] = np.float32(0)
+	convolution_window_morph[1,2] = np.float32(0)
+	convolution_window_morph[2,1] = np.float32(0)
+
+	convolution_window_morph = cl.Buffer(wrp.ocl.context , cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=convolution_window_morph)
+
 	convolution_window_2d = np.ones((5, 5), np.float32) * (1/25)
 	convolution_window_cl = cl.Buffer(wrp.ocl.context , cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=convolution_window_2d)
 	
 	#wrp.vglClBlurSq3(img_input, img_output)
-	wrp.vglClConvolution(img_input, img_output, convolution_window_cl, np.uint32(5), np.uint32(5))
+	#wrp.vglClConvolution(img_input, img_output, convolution_window_cl, np.uint32(5), np.uint32(5))
+	#wrp.vglClCopy(img_input, img_output)
+	wrp.vglClConvolution(img_input_morph, img_output_morph, convolution_window_morph, np.uint32(3), np.uint32(3))
 	
-	vl.vglClDownload(img_output)
+	vl.vglClDownload(img_output_morph)
+	#vl.vglClDownload(img_output)
+	
 
 	# SAVING IMAGE img_output
 	ext = sys.argv[2].split(".")
@@ -140,8 +261,9 @@ if __name__ == "__main__":
 	if( ext.pop(0).lower() == 'jpg' ):
 		if( img_output.getVglShape().getNChannels() == 4 ):
 			vl.rgba_to_rgb(img_output)
-	
-	vl.vglSaveImage(sys.argv[2], img_output)
+
+	vl.vglSaveImage(sys.argv[2], img_output_morph)
+	#vl.vglSaveImage(sys.argv[2], img_output)
 
 
 """
