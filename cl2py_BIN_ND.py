cb3a324918ef2f8adb4f98c6a5f9a20b96ff7f1d
Artur Santos Nascimento <arturxz@outlook.com.br>
2019-02-26 00:39:34 -0300

    cl2py_BIN_ND parcially working

--- cl2py_BIN_ND.py
+++ cl2py_BIN_ND.py
@@ -61,11 +61,10 @@ class cl2py_BIN_ND:
 		if( img_output.depth == vl.IPL_DEPTH_1U() ):
 			_worksize_0 = img_output.getWidthStep()
 		
-		worksize = (np.int32(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
-		print("::::::::::::::::::::::::::", worksize)
+		worksize = (int(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
 		# ENQUEUEING KERNEL EXECUTION
-		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
-		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
+		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
 		
 		vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
 
@@ -104,11 +103,11 @@ class cl2py_BIN_ND:
 		if( img_output.depth == vl.IPL_DEPTH_1U() ):
 			_worksize_0 = img_output.getWidthStep()
 		
-		worksize = (np.int32(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
+		worksize = (int(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
 				
 		# ENQUEUEING KERNEL EXECUTION
-		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
-		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
+		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
 		
 		vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
 
@@ -147,11 +146,11 @@ class cl2py_BIN_ND:
 		if( img_output.depth == vl.IPL_DEPTH_1U() ):
 			_worksize_0 = img_output.getWidthStep()
 		
-		worksize = (np.int32(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
+		worksize = (int(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
 				
 		# ENQUEUEING KERNEL EXECUTION
-		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
-		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
+		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
 		
 		vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
 
@@ -190,11 +189,11 @@ class cl2py_BIN_ND:
 		if( img_output.depth == vl.IPL_DEPTH_1U() ):
 			_worksize_0 = img_output.getWidthStep()
 		
-		worksize = (np.int32(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
+		worksize = (int(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
 				
 		# ENQUEUEING KERNEL EXECUTION
-		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
-		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
+		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
 		
 		vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
 
@@ -204,7 +203,7 @@ class cl2py_BIN_ND:
 			print("vglClNdBinMax: Error: this function supports only OpenCL data as buffer and img_input isn't.")
 			exit()
 		if( not img_input2.clForceAsBuf == vl.IMAGE_ND_ARRAY() ):
-			print("vglClNdBinMax: Error: this function supports only OpenCL data as buffer and img_input isn't.")
+			print("vglClNdBinMax: Error: this function supports only OpenCL data as buffer and img_input2 isn't.")
 			exit()
 		if( not img_output.clForceAsBuf == vl.IMAGE_ND_ARRAY() ):
 			print("vglClNdBinMax: Error: this function supports only OpenCL data as buffer and img_output isn't.")
@@ -233,10 +232,11 @@ class cl2py_BIN_ND:
 		if( img_output.depth == vl.IPL_DEPTH_1U() ):
 			_worksize_0 = img_output.getWidthStep()
 		
-		worksize = (np.int32(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
+		worksize = (int(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
 				
 		# ENQUEUEING KERNEL EXECUTION
-		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
 		
 		vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
 
@@ -275,10 +275,11 @@ class cl2py_BIN_ND:
 		if( img_output.depth == vl.IPL_DEPTH_1U() ):
 			_worksize_0 = img_output.getWidthStep()
 		
-		worksize = (np.int32(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
+		worksize = (int(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
 				
 		# ENQUEUEING KERNEL EXECUTION
-		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
 		
 		vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
 
@@ -298,8 +299,8 @@ class cl2py_BIN_ND:
 			print("vglClNdNot: Error: window is not a VglStrEl object. aborting execution.")
 			exit()
 
-		_program = self.cl_ctx.get_compiled_kernel("../CL_ND/vglClNdNot.cl", "vglClNdNot")
-		kernel_run = _program.vglClNdNot
+		_program = self.cl_ctx.get_compiled_kernel("../CL_BIN/vglClNdBinNot.cl", "vglClNdBinNot")
+		kernel_run = _program.vglClNdBinNot
 
 		kernel_run.set_arg(0, img_input.get_oclPtr())
 		kernel_run.set_arg(1, img_output.get_oclPtr())
@@ -310,13 +311,15 @@ class cl2py_BIN_ND:
 		if( img_output.depth == vl.IPL_DEPTH_1U() ):
 			_worksize_0 = img_output.getWidthStep()
 		
-		worksize = (_worksize_0, img_input.getHeigthIn(), img_input.getNFrames() )
+		worksize = (int(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
 				
 		# ENQUEUEING KERNEL EXECUTION
-		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
-		
+		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
+
 		vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
 
+	"""
 	def vglClNdBinRoi(self, img_input, p0, pf):
 
 		if( not img_input.clForceAsBuf == vl.IMAGE_ND_ARRAY() ):
@@ -364,12 +367,14 @@ class cl2py_BIN_ND:
 		if( img_output.depth == vl.IPL_DEPTH_1U() ):
 			_worksize_0 = img_output.getWidthStep()
 		
-		worksize = (np.int32(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
+		#worksize = (int(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
 				
 		# ENQUEUEING KERNEL EXECUTION
-		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
-		
+		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.get_ipl().shape, None)
+
 		vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
+	"""
 
 	def vglClNdBinThreshold(self, img_input, img_output, thresh):
 
@@ -410,10 +415,48 @@ class cl2py_BIN_ND:
 		if( img_output.depth == vl.IPL_DEPTH_1U() ):
 			_worksize_0 = img_output.getWidthStep()
 		
-		worksize = (np.int32(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
+		#worksize = (int(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
 				
 		# ENQUEUEING KERNEL EXECUTION
-		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.ipl.shape, None)
+
+		vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
+
+	def vglClNdBinToGray(self, img_input, img_output):
+
+		if( not img_input.clForceAsBuf == vl.IMAGE_ND_ARRAY() ):
+			print("vglClNdBinToGray: Error: this function supports only OpenCL data as buffer and img_input isn't.")
+			exit()
+		if( not img_output.clForceAsBuf == vl.IMAGE_ND_ARRAY() ):
+			print("vglClNdBinToGray: Error: this function supports only OpenCL data as buffer and img_output isn't.")
+			exit()
+
+		vl.vglCheckContext(img_input, vl.VGL_CL_CONTEXT())
+		vl.vglCheckContext(img_output, vl.VGL_CL_CONTEXT())
+
+		_program = self.cl_ctx.get_compiled_kernel("../CL_BIN/vglClNdBinToGray.cl", "vglClNdBinToGray")
+		kernel_run = _program.vglClNdBinToGray
+
+		mobj_img_shape_input = img_input.getVglShape().get_asVglClShape_buffer()
+		mobj_img_shape_output = img_output.getVglShape().get_asVglClShape_buffer()
+
+		kernel_run.set_arg(0, img_input.get_oclPtr())
+		kernel_run.set_arg(1, img_output.get_oclPtr())
+		kernel_run.set_arg(2, mobj_img_shape_input)
+		kernel_run.set_arg(3, mobj_img_shape_output)
+				
+		_worksize_0 = img_input.getWidthIn()
+		if( img_input.depth == vl.IPL_DEPTH_1U() ):
+			_worksize_0 = img_input.getWidthStep()
+		if( img_output.depth == vl.IPL_DEPTH_1U() ):
+			_worksize_0 = img_output.getWidthStep()
+		
+		#worksize = (np.int32(_worksize_0), img_input.getHeigthIn(), img_input.getNFrames() )
+				
+		# ENQUEUEING KERNEL EXECUTION
+		#cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, worksize, None)
+		cl.enqueue_nd_range_kernel(self.ocl.commandQueue, kernel_run, img_output.ipl.shape, None)
 		
 		vl.vglSetContext(img_output, vl.VGL_CL_CONTEXT())
 
@@ -425,11 +468,11 @@ if __name__ == "__main__":
 	wrp = cl2py_BIN_ND()
 
 	# INPUT IMAGE
-	img_input = vl.VglImage("bin.pgm", vl.VGL_IMAGE_2D_IMAGE(), vl.IMAGE_ND_ARRAY())
+	img_input = vl.VglImage("bin.pgm", vl.VGL_IMAGE_2D_IMAGE(), None, vl.IMAGE_ND_ARRAY())
 	vl.vglLoadImage(img_input)
 	vl.vglClUpload(img_input)
 
-	img_input2 = vl.VglImage("bin2.pgm", vl.VGL_IMAGE_2D_IMAGE(), vl.IMAGE_ND_ARRAY())
+	img_input2 = vl.VglImage("bin2.pgm", vl.VGL_IMAGE_2D_IMAGE(), None, vl.IMAGE_ND_ARRAY())
 	vl.vglLoadImage(img_input2)
 	vl.vglClUpload(img_input2)
 
@@ -438,49 +481,126 @@ if __name__ == "__main__":
 	img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
 	vl.vglAddContext(img_output, vl.VGL_CL_CONTEXT())
 
+	img_out_aux = vl.get_similar_oclPtr_object(img_input)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_out_aux, img_output.get_oclPtr())
+
+	# TRANSFORMANDO IMAGENS EM IMAGENS BINARIAS
+	wrp.vglClNdBinThreshold(img_input, img_output, 100)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
+
 	# STRUCTURANT ELEMENT
 	window = vl.VglStrEl()
 	window.constructorFromTypeNdim(vl.VGL_STREL_CROSS(), 2)
-	"""
+	
+	# INPUT IMAGE
+	img_input = vl.VglImage("bin.pgm", vl.VGL_IMAGE_2D_IMAGE(), None, vl.IMAGE_ND_ARRAY())
+	vl.vglLoadImage(img_input)
+	vl.vglClUpload(img_input)
+	wrp.vglClNdBinThreshold(img_input, img_output, 100)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
+
 	wrp.vglClNdBinDilate(img_input, img_output, window)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
+	wrp.vglClNdBinToGray(img_input, img_output)
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-	vl.vglSaveImage("bin-vglClNdBinDilate.pgm", img_output)
+	vl.vglSaveImage("bin-vglClNdBinDilate.png", img_output)
+	img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
+	
+	img_input = vl.VglImage("bin.pgm", vl.VGL_IMAGE_2D_IMAGE(), None, vl.IMAGE_ND_ARRAY())
+	vl.vglLoadImage(img_input)
+	vl.vglClUpload(img_input)
+	wrp.vglClNdBinThreshold(img_input, img_output, 100)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
 
 	wrp.vglClNdBinDilatePack(img_input, img_output, window)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
+	wrp.vglClNdBinToGray(img_input, img_output)
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-	vl.vglSaveImage("bin-vglClNdBinDilatePack.pgm", img_output)
+	vl.vglSaveImage("bin-vglClNdBinDilatePack.png", img_output)
+	img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
+
+	img_input = vl.VglImage("bin.pgm", vl.VGL_IMAGE_2D_IMAGE(), None, vl.IMAGE_ND_ARRAY())
+	vl.vglLoadImage(img_input)
+	vl.vglClUpload(img_input)
+	wrp.vglClNdBinThreshold(img_input, img_output, 100)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
 
 	wrp.vglClNdBinErode(img_input, img_output, window)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
+	wrp.vglClNdBinToGray(img_input, img_output)
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-	vl.vglSaveImage("bin-vglClNdBinErode.pgm", img_output)
+	vl.vglSaveImage("bin-vglClNdBinErode.png", img_output)
+	img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
+
+	img_input = vl.VglImage("bin.pgm", vl.VGL_IMAGE_2D_IMAGE(), None, vl.IMAGE_ND_ARRAY())
+	vl.vglLoadImage(img_input)
+	vl.vglClUpload(img_input)
+	wrp.vglClNdBinThreshold(img_input, img_output, 100)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
 
 	wrp.vglClNdBinErodePack(img_input, img_output, window)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
+	wrp.vglClNdBinToGray(img_input, img_output)
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-	vl.vglSaveImage("bin-vglClNdBinErodePack.pgm", img_output)
+	vl.vglSaveImage("bin-vglClNdBinErodePack.png", img_output)
+	img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
+
+	img_input = vl.VglImage("bin.pgm", vl.VGL_IMAGE_2D_IMAGE(), None, vl.IMAGE_ND_ARRAY())
+	vl.vglLoadImage(img_input)
+	vl.vglClUpload(img_input)
+	wrp.vglClNdBinThreshold(img_input, img_output, 100)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
 
 	wrp.vglClNdBinMax(img_input, img_input2, img_output)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
+	wrp.vglClNdBinToGray(img_input, img_output)
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-	vl.vglSaveImage("bin-vglClNdBinMax.pgm", img_output)
+	vl.vglSaveImage("bin-vglClNdBinMax.png", img_output)
+	img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
+
+	img_input = vl.VglImage("bin.pgm", vl.VGL_IMAGE_2D_IMAGE(), None, vl.IMAGE_ND_ARRAY())
+	vl.vglLoadImage(img_input)
+	vl.vglClUpload(img_input)
+	wrp.vglClNdBinThreshold(img_input, img_output, 100)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
 
 	wrp.vglClNdBinMin(img_input, img_input2, img_output)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
+	wrp.vglClNdBinToGray(img_input, img_output)
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-	vl.vglSaveImage("bin-vglClNdBinMin.pgm", img_output)
+	vl.vglSaveImage("bin-vglClNdBinMin.png", img_output)
+	img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
+
+	img_input = vl.VglImage("bin.pgm", vl.VGL_IMAGE_2D_IMAGE(), None, vl.IMAGE_ND_ARRAY())
+	vl.vglLoadImage(img_input)
+	vl.vglClUpload(img_input)
+	wrp.vglClNdBinThreshold(img_input, img_output, 100)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
 
 	wrp.vglClNdBinNot(img_input, img_output)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
+	wrp.vglClNdBinToGray(img_input, img_output)
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-	vl.vglSaveImage("bin-vglClNdBinNot.pgm", img_output)
+	vl.vglSaveImage("bin-vglClNdBinNot.png", img_output)
+	img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
+
+	"""
+	img_input = vl.VglImage("bin.pgm", vl.VGL_IMAGE_2D_IMAGE(), None, vl.IMAGE_ND_ARRAY())
+	vl.vglLoadImage(img_input)
+	vl.vglClUpload(img_input)
+	wrp.vglClNdBinThreshold(img_input, img_output, 100)
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
 
 	wrp.vglClNdBinRoi(img_output, np.uint32((25, 25)), np.uint32((75, 75)))
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
+	cl.enqueue_copy(wrp.cl_ctx.queue, img_input.get_oclPtr(), img_output.get_oclPtr())
+	wrp.vglClNdBinToGray(img_input, img_output)
 	vl.vglSaveImage("bin-vglClNdBinRoi.pgm", img_output)
 	"""
-	wrp.vglClNdBinThreshold(img_input, img_output, 100)
-	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-	vl.vglSaveImage("bin-vglClNdBinThreshold.jpg", img_output)
 
-	wrp.vglClNdBinThreshold(img_input, img_output, 100)
-	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-	vl.vglSaveImage("bin-vglClNdBinThreshold.jpg", img_output)
+	#wrp.vglClNdBinToGray(img_input, img_output)
+	#vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
+	#vl.vglSaveImage("bin-vglClNdBinToGray.pgm", img_output)
 
 	wrp = None
 	img_input = None
