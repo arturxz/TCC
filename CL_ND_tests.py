17d2ce506fa3e29add2a46902edb587b436673ec
Artur Santos Nascimento <arturxz@outlook.com.br>
2019-04-03 00:10:03 -0300

    wrapper testers now are benchmarkers

--- CL_ND_tests.py
+++ CL_ND_tests.py
@@ -12,12 +12,16 @@ import numpy as np
 # IMPORTING METHODS
 from cl2py_ND import * 
 
+import time as t
+
 """
 	HERE FOLLOWS THE KERNEL CALLS
 """
 if __name__ == "__main__":
 	
 	vl.vglClInit()
+
+	msg = ""
 
 	# INPUT IMAGE
 	img_input = vl.VglImage("img-1.jpg", None, vl.VGL_IMAGE_2D_IMAGE(), vl.IMAGE_ND_ARRAY())
@@ -33,29 +37,51 @@ if __name__ == "__main__":
 	window = vl.VglStrEl()
 	window.constructorFromTypeNdim(vl.VGL_STREL_CROSS(), 2)
 
+	inicio = t.time()
 	vglClNdCopy(img_input, img_output)
+	fim = t.time()
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
 	vl.vglSaveImage("yamamoto-vglNdCopy.jpg", img_output)
+	msg = msg + "Tempo de execução do método vglClNdCopy:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"
 
+	inicio = t.time()
 	vglClNdConvolution(img_input, img_output, window)
+	fim = t.time()
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
 	vl.vglSaveImage("yamamoto-vglNdConvolution.jpg", img_output)
-	
+	msg = msg + "Tempo de execução do método vglClNdConvolution:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"
+
+	inicio = t.time()
 	vglClNdDilate(img_input, img_output, window)
+	fim = t.time()
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
 	vl.vglSaveImage("yamamoto-vglNdDilate.jpg", img_output)
+	msg = msg + "Tempo de execução do método vglClNdDilate:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"
 
+	inicio = t.time()
 	vglClNdErode(img_input, img_output, window)
+	fim = t.time()
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
 	vl.vglSaveImage("yamamoto-vglNdErode.jpg", img_output)
+	msg = msg + "Tempo de execução do método vglClNdErode:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"
 
+	inicio = t.time()
 	vglClNdNot(img_input, img_output)
+	fim = t.time()
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
 	vl.vglSaveImage("yamamoto-vglNdNot.jpg", img_output)
+	msg = msg + "Tempo de execução do método vglClNdNot:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"
 
+	inicio = t.time()
 	vglClNdThreshold(img_input, img_output, np.uint8(120), np.uint8(190))
+	fim = t.time()
 	vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
 	vl.vglSaveImage("yamamoto-vglNdThreshold.jpg", img_output)
+	msg = msg + "Tempo de execução do método vglClNdThreshold:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"
+
+	print("-------------------------------------------------------------")
+	print(msg)
+	print("-------------------------------------------------------------")
 
 	wrp = None
 	img_input = None
--- CL_tests.py
+++ CL_tests.py
@@ -12,6 +12,7 @@ import numpy as np
 # IMPORTING METHODS
 from cl2py_shaders import * 
 
+import time as t
 
 """
 	HERE FOLLOWS THE KERNEL CALLS
@@ -36,6 +37,8 @@ if __name__ == "__main__":
 	"""
 		CL.IMAGE OBJECTS
 	"""
+
+	msg = ""
 
 	vl.vglClInit()
 
@@ -84,87 +87,161 @@ if __name__ == "__main__":
 	morph_window_3d[1,2,1] = 255
 	morph_window_3d[2,1,1] = 255
 
+	inicio = t.time()
 	vglClBlurSq3(img_input, img_output)
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClBlurSq3.jpg")
 	vl.rgb_to_rgba(img_output)
-	
+	msg = msg + "Tempo de execução do método vglClBlurSq3:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
+
+	inicio = t.time()
 	vglClConvolution(img_input, img_output, convolution_window_2d, np.uint32(5), np.uint32(5))
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClConvolution.jpg")
 	vl.rgb_to_rgba(img_output)
+	msg = msg + "Tempo de execução do método vglClConvolution:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglClCopy(img_input, img_output)
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClCopy.jpg")
 	vl.rgb_to_rgba(img_output)
+	msg = msg + "Tempo de execução do método vglClCopy:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglClDilate(img_input, img_output, morph_window_2d, np.uint32(3), np.uint32(3))
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClDilate.jpg")
 	vl.rgb_to_rgba(img_output)
+	msg = msg + "Tempo de execução do método vglClDilate:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglClErode(img_input, img_output, morph_window_2d, np.uint32(3), np.uint32(3))
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClErode.jpg")
 	vl.rgb_to_rgba(img_output)
+	msg = msg + "Tempo de execução do método vglClErode:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglClInvert(img_input, img_output)
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClInvert.jpg")
 	vl.rgb_to_rgba(img_output)
+	msg = msg + "Tempo de execução do método vglClInvert:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglClMax(img_input, img_input2, img_output)
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClMax.jpg")
 	vl.rgb_to_rgba(img_output)
+	msg = msg + "Tempo de execução do método vglClMax:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglClMin(img_input, img_input2, img_output)
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClMin.jpg")
 	vl.rgb_to_rgba(img_output)
+	msg = msg + "Tempo de execução do método vglClMin:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglClSub(img_input, img_input2, img_output)
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClSub.jpg")
 	vl.rgb_to_rgba(img_output)
+	msg = msg + "Tempo de execução do método vglClSub:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglClSum(img_input, img_input2, img_output)
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClSum.jpg")
 	vl.rgb_to_rgba(img_output)
+	msg = msg + "Tempo de execução do método vglClSum:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglClSwapRgb(img_input, img_output)
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClSwapRgb.jpg")
 	vl.rgb_to_rgba(img_output)
+	msg = msg + "Tempo de execução do método vglClSwapRgb:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglClThreshold(img_input, img_output, np.float32(0.5), np.float32(0.9))
+	fim = t.time()
 	salvando2d(img_output, "yamamoto-vglClThreshold.jpg")
 	vl.rgb_to_rgba(img_output)
-	
+	msg = msg + "Tempo de execução do método vglClThreshold:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
+
+	inicio = t.time()
 	vglCl3dBlurSq3(img_input_3d, img_output_3d)
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dBlurSq3.tif")
+	msg = msg + "Tempo de execução do método vglCl3dBlurSq3:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglCl3dConvolution(img_input_3d, img_output_3d, convolution_window_3d, np.uint32(5), np.uint32(5), np.uint32(5))
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dConvolution.tif")
+	msg = msg + "Tempo de execução do método vglCl3dConvolution:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglCl3dCopy(img_input_3d, img_output_3d)
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dCopy.tif")
+	msg = msg + "Tempo de execução do método vglCl3dCopy:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglCl3dDilate(img_input_3d, img_output_3d, morph_window_3d, np.uint32(3), np.uint32(3), np.uint32(3))
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dDilate.tif")
+	msg = msg + "Tempo de execução do método vglCl3dDilate:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglCl3dErode(img_input_3d, img_output_3d, morph_window_3d, np.uint32(3), np.uint32(3), np.uint32(3))
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dErode.tif")
+	msg = msg + "Tempo de execução do método vglCl3dErode:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglCl3dNot(img_input_3d, img_output_3d)
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dNot.tif")
+	msg = msg + "Tempo de execução do método vglCl3dNot:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglCl3dMax(img_input_3d, img_input2_3d, img_output_3d)
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dMax.tif")
+	msg = msg + "Tempo de execução do método vglCl3dMax:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglCl3dMin(img_input_3d, img_input2_3d, img_output_3d)
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dMin.tif")
+	msg = msg + "Tempo de execução do método vglCl3dMin:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglCl3dSub(img_input_3d, img_input2_3d, img_output_3d)
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dSub.tif")
+	msg = msg + "Tempo de execução do método vglCl3dSub:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglCl3dSum(img_input_3d, img_input2_3d, img_output_3d)
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dSum.tif")
+	msg = msg + "Tempo de execução do método vglCl3dSum:\t\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
 
+	inicio = t.time()
 	vglCl3dThreshold(img_input_3d, img_output_3d, np.float32(0.4), np.float32(.8))
+	fim = t.time()
 	salvando2d(img_output_3d, "3d-vglCl3dThreshold.tif")
-	
+	msg = msg + "Tempo de execução do método vglCl3dThreshold:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"	
+
+	print("-------------------------------------------------------------")
+	print(msg)
+	print("-------------------------------------------------------------")
+
+
 	img_input = None
 	img_input_3d = None
 
